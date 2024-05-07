5ed37fbdea810675ff4230e3f3157e9c92e25d2a
mehrubonkhusaynov <husaynov888@gmail.com>
2024-05-08 00:45:27 +0500

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by Mehrubon Khusaynov on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
